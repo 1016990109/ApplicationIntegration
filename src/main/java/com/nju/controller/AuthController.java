9a3f997d5369dabdf32416853c2b5e3b26b898a0
1016990109 <1016990109@qq.com>
2016-05-23 16:38:55 +0800

    Initial commit

---
+++ src/main/java/com/nju/controller/AuthController.java
@@ -0,0 +1,31 @@
+package com.nju.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by 传旺 on 2016/5/23.
+ */
+
+@Controller
+public class AuthController {
+
+    @RequestMapping(value = "/login" , method = RequestMethod.POST)
+    public String login(){
+        return "/login";
+    }
+
+    @RequestMapping(value = "/login" , method = RequestMethod.POST)
+    @ResponseBody
+    public Map<String, Object> postlogin(String username, String password, HttpSession session){
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("test","test");
+        return map;
+    }
+}
---
+++ src/main/webapp/pages/login.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 传旺
+  Date: 2016/5/23
+  Time: 16:11
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>登录</title>
+</head>
+<body>
+    <form action="/login">
+        <label for="">username</label><input type="text">
+        <label for="">password</label><input type="password">
+    </form>
+</body>
+</html>
